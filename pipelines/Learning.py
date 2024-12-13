df9771d68a0403de04e95ab0f14a3edb05a1aae8
locch <locc7196@gmail.com>
2024-12-13 21:10:44 +0700

    add new pipeline

--- pipelines/Learning.py
+++ pipelines/Learning.py
@@ -26,14 +26,77 @@ class BasePipeline:
         self.top_p = top_p
         self.token_length = token_length
 
+# class NameOfPipeline(BasePipeline):
+#
+#     def __call__(self, content: str) -> Optional[List[Dict[str, str]]]:
+#         try:
+#             pass
+#         except Exception as e:
+#             raise RuntimeError(f"An error occurred while processing the content: {e}")
+
 class TroubleshootingPipeline(BasePipeline):
 
     def __call__(self, content: str) -> Optional[List[Dict[str, str]]]:
-        pass
+
+        try:
+            # Generate Question
+            messages = [
+                {"role": "system", "content": "You are an expert in creating clear and insightful questions specifically about troubleshooting technical issues."},
+                {"role": "user", "content": "Below is the content for generating a question:"},
+                {"role": "user", "content": content},
+                {"role": "user", "content": (
+                    "Based on the above content, generate a question that focuses specifically on troubleshooting the issue described."
+                    " The question should be clear, concise, and focused on testing the ability to apply troubleshooting methods and techniques."
+                    " The question should provide enough context for someone to answer it without needing to refer back to the content."
+                    " Please provide only the question in your response, without any extra information or explanation."
+                )}
+            ]
+
+            response = self.client.chat.completions.create(
+                messages=messages,
+                model=self.model,
+                temperature=random.uniform(self.temp_min, self.temp_max),
+                max_tokens=self.token_length,
+                top_p=self.top_p
+            )
+
+            question = response.choices[0].message.content
+
+            # Generate Answer
+            messages = [
+                {"role": "system", "content": "You are an expert who answers questions clearly and in a natural, conversational style."},
+                {"role": "user", "content": "Please provide a thoughtful and detailed response to the following troubleshooting question:"},
+                {"role": "user", "content": f"{question}"}
+            ]
+
+            response = self.client.chat.completions.create(
+                messages=messages,
+                model=self.model,
+                temperature=random.uniform(self.temp_min, self.temp_max),
+                max_tokens=self.token_length,
+                top_p=self.top_p
+            )
+            
+            answer = response.choices[0].message.content
+
+            # Generate Instruction data
+            instruct_data = [
+                {"role": "system", "content": "You are a programming expert."},
+                {"role": "user", "content": "Please answer the following question."},
+                {"role": "user", "content": f"{question}"},
+                {"role": "user", "content": f"{answer}"},
+            ]
+
+            return instruct_data
+
+        except Exception as e:
+            raise RuntimeError(f"An error occurred while processing the content: {e}")
+
 
 class DefinitionPipeline(BasePipeline):
 
     def __call__(self, content: str) -> Optional[List[Dict[str, str]]]:
+
         try:
             # Generate Question
             messages = [
