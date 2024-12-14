947725d16ea467bb81ce06956145083180bcdcb1
locch <locc7196@gmail.com>
2024-12-14 08:20:04 +0700

    update

---
+++ pipelines/Coding.py
@@ -0,0 +1,37 @@
+import random
+from typing import Any, List, Dict, Optional
+
+class BasePipeline:
+    """
+    Base pipeline for inheritance
+    """
+
+    def __init__(self, client: Any, model: str = "gpt-4o", temp_min: float = 0.4, 
+                temp_max: float = 0.7, top_p: float = 0.8, token_length: int = 512):
+        """
+        Initializes the Pipeline with the provided parameters.
+
+        Args:
+            client (Any): The client object for interacting with the language model API.
+            model (str): The model to use for generating responses (default: "gpt-4o").
+            temp_min (float): Minimum temperature for randomness in response (default: 0.4).
+            temp_max (float): Maximum temperature for randomness in response (default: 0.7).
+            top_p (float): The top_p value for nucleus sampling (default: 0.8).
+            token_length (int): The maximum token length for the generated responses (default: 512).
+        """
+        self.client = client
+        self.model = model
+        self.temp_min = temp_min
+        self.temp_max = temp_max
+        self.top_p = top_p
+        self.token_length = token_length
+
+
+# class NameOfPipeline(BasePipeline):
+#
+#     def __call__(self, content: str) -> Optional[List[Dict[str, str]]]:
+#
+#         try:
+#             pass
+#         except Exception as e:
+#             raise RuntimeError(f"An error occurred while processing the content: {e}")
